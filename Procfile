554897ba4d4ecdf6c7c1616a0ab1e3d19d8bcffd
timochfati <lamrini788@gmail.com>
2019-08-02 18:20:51 +0000

    added to a Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn django_todo.wsgi:application
+web: gunicorn ecommerce.wsgi:application
--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
-    'django_todo',
     'accounts',
     'products',
     'cart',
