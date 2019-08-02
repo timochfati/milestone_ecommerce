44d85d418c2f9a5efb041c01cfb7f7c919b89ea8
timochfati <lamrini788@gmail.com>
2019-08-02 17:31:34 +0000

    added a Procfile

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import env 
 import dj_database_url
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
